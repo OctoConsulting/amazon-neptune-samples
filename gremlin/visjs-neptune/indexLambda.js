ac3a6d4878f6c5070f214340d3eef30f6b25ee7a
George Batchvarov <georgebatchvarov@George-Batchvarov-MacBook-Pro.local>
2019-11-20 12:47:35 -0500

    Generalized version of the graph visualization

--- gremlin/visjs-neptune/indexLambda.js
+++ gremlin/visjs-neptune/indexLambda.js
@@ -1,27 +1,3 @@
-/*
- * MIT License
-
-* Copyright (c) 2018.  Amazon Web Services, Inc. All Rights Reserved.
-
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-
-* The above copyright notice and this permission notice shall be included in all
-* copies or substantial portions of the Software.
-
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-**/
-
 var gremlin = require('gremlin');
 var http = require('http');
 var url = require('url');
@@ -33,7 +9,7 @@ exports.handler = function(event, context, callback) {
     var DriverRemoteConnection = gremlin.driver.DriverRemoteConnection;
     var Graph = gremlin.structure.Graph;
     //Use wss:// for secure connections. See https://docs.aws.amazon.com/neptune/latest/userguide/access-graph-ssl.html 
-    dc = new DriverRemoteConnection('wss://'+process.env.NEPTUNE_CLUSTER_ENDPOINT+':'+process.env.NEPTUNE_PORT+'/gremlin');
+    var dc = new DriverRemoteConnection('wss://'+process.env.NEPTUNE_CLUSTER_ENDPOINT+':'+process.env.NEPTUNE_PORT+'/gremlin');
     var graph = new Graph();
     var g = graph.traversal().withRemote(dc);
 
@@ -50,37 +26,8 @@ exports.handler = function(event, context, callback) {
     console.log("event.pathParameters.proxy => "+ event.pathParameters.proxy);
     console.log(event.pathParameters.proxy.match(/proxy/ig));
 
-    // this code is only for populating the search LoV
-    if (event.pathParameters.proxy.match(/initialize/ig)) {
-        //using another technique as opposed to creating a new callback function
-
-        g.V().hasLabel('User').limit(1000).valueMap(true).toList().then(
-            data => {
-            console.log("Response from Neptune for initialize .." + JSON.stringify(data));
-        var nodes=[];
-        for(var i = 0;    i < data.length;    i++)
-        {
-            nodes.push({name: data[i].name.toString()});
-        }
-        var response = {
-            statusCode: 200,
-            headers: headers,
-            body: JSON.stringify(nodes)
-        };
-        console.log("Initialize call response: " + JSON.stringify(data));
-        callback(null, response);
-        context.done();
-        dc.close(); // look at this carefully!!!
-    }).
-        catch(error => {
-            console.log('ERROR', error);
-        dc.close();
-    });
-    }
-
-
     if (event.pathParameters.proxy.match(/search/ig)) {
-        g.V().has('name', gremlin.process.P.between(event.queryStringParameters.username, event.queryStringParameters.touser)).limit(20).valueMap(true).toList().then(
+        g.V().has(event.queryStringParameters.nodetype,event.queryStringParameters.field, gremlin.process.P.between(event.queryStringParameters.from, event.queryStringParameters.to)).limit(20).valueMap(true).toList().then(
             data => {
             console.log(JSON.stringify(data));
             var response = {
@@ -99,9 +46,8 @@ exports.handler = function(event, context, callback) {
     });
     }
 
-
-    if (event.pathParameters.proxy.match(/neighbours/ig)) {
-        g.V().has('User','~id',event.queryStringParameters.id).in_('Follows').valueMap(true).limit(10).toList().then(
+    if (event.pathParameters.proxy.match(/neighbours-out/ig)) {
+        g.V().has(event.queryStringParameters.nodetype,'~id',event.queryStringParameters.id).out(event.queryStringParameters.edgetype).valueMap(true).toList().then(
             data => {
             console.log(JSON.stringify(data));
         var response = {
@@ -121,30 +67,8 @@ exports.handler = function(event, context, callback) {
 
     }
 
-
-    if (event.pathParameters.proxy.match(/getusertweets/ig)) {
-        g.V().has('User', '~id', event.queryStringParameters.userid).out('Tweets').limit(3).valueMap(true).toList().then(
-            data => {
-        console.log("getusertweets data" + JSON.stringify(data));
-        var response = {
-            statusCode: 200,
-            headers: headers,
-            body: JSON.stringify(data)
-        };
-        console.log("getusertweets response: " + JSON.stringify(data));
-        callback(null, response);
-        context.done();
-        dc.close(); // look at this carefully!!!
-        }).
-        catch(error => {
-            console.log('ERROR', error);
-        dc.close();
-        });
-    }
-
-
-    if (event.pathParameters.proxy.match(/whichusersliketweet/ig)) {
-        g.V().has('Tweet','~id',event.queryStringParameters.tweetid).in_('Likes').hasLabel('User').limit(5).valueMap(true).toList().then(
+    if (event.pathParameters.proxy.match(/neighbours-in/ig)) {
+        g.V().has(event.queryStringParameters.nodetype,'~id',event.queryStringParameters.id).in_(event.queryStringParameters.edgetype).valueMap(true).toList().then(
             data => {
             console.log(JSON.stringify(data));
         var response = {
@@ -152,17 +76,19 @@ exports.handler = function(event, context, callback) {
             headers: headers,
             body: JSON.stringify(data)
         };
-        console.log("getusertweets response: " + JSON.stringify(data));
+        console.log("getNeighbours response: " + JSON.stringify(data));
         callback(null, response);
         context.done();
-        dc.close(); // look at this carefully!!!
+        dc.close();
     }).
         catch(error => {
             console.log('ERROR', error);
         dc.close();
     });
+
     }
 
-
 }
+
+
 
